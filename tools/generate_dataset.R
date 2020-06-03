33093f729cc4aa9bf945607d844f6c7c398e1932
RobertaSirovich <roberta.sirovich@unito.it>
2020-06-03 10:11:42 +0200

    Small Changes: no Maelle poster, yes Alessio Crippa poster.

--- tools/generate_dataset.R
+++ tools/generate_dataset.R
@@ -191,7 +191,7 @@ accepted_reduced <- accepted %>%
 eventbrite_reduced <- eventbrite %>% 
   mutate(TipologiaBiglietto = `Tipologia biglietto`) %>%
   filter(`Tipologia biglietto` == "Conference ticket - Speaker" 
-         | Cognome == "Salmon" 
+         | Cognome == "Crippa" 
          | Cognome == "Ryser-Welch"
          | Cognome == "Marini"
          | Cognome == "Melloncelli"
@@ -267,7 +267,7 @@ session_speakers_confirmed <- full_join(all_sessions_accepted, all_speakers_conf
 # eventbrite_reduced[str_detect(eventbrite_reduced$NameSurname,"KIRILL"),1]
 # all_speakers_reduced[str_detect(all_speakers_reduced$NameSurname,"KIRILL"),2]
 # session_speakers_confirmed[str_detect(session_speakers_confirmed$namesurname,"CORRADIN"),2]
-
+filter(all_speakers_confirmed,TipologiaBiglietto != "Conference ticket - Speaker") -> ToAddEventbrite
 
 
 #Manual fix remove broken link
