53170f84a720e6648723b87820313ab3d9e48e40
riccardoporreca <riccardo.porreca@mirai-solutions.com>
2020-08-27 23:51:18 +0200

    Make external links opening in a new tab
    * This is nicer and avoids browser security issues when embedding the page (e.g. in the main e-Rum2020 website).

--- tools/render-conference-materials.R
+++ tools/render-conference-materials.R
@@ -1,4 +1,6 @@
 output_dir <- "_site"
+
+# create .md file for the conference materials page, extracted from README.md
 readme <- readLines("README.md")
 materials_md <- c(
   # include heading
@@ -8,5 +10,16 @@ materials_md <- c(
 )
 md_file <- file.path(output_dir, "conference-materials.md")
 writeLines(materials_md, md_file)
+
+# render to HTML as GitHub document
 rmarkdown::render(md_file, "github_document")
 file.remove(md_file)
+
+# make external links opening in a new tab, to also avoid browser security
+# issues when embedding the page
+html_file <- sub(".md", ".html", md_file, fixed = TRUE)
+materials_html <- xml2::read_html(html_file)
+links <- rvest::html_nodes(materials_html, css = "a")
+internal <- grepl("^[#]", xml2::xml_attr(links, "href"))
+xml2::xml_attr(links[!internal], "target") <- "_blank"
+xml2::write_html(materials_html, html_file)
